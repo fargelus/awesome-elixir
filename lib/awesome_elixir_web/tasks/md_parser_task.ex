41b204159ac4debeb67eda05a70a79b78169134e
fargelus <ddraudred@gmail.com>
2022-07-12 19:56:33 +0300

    Colorized outdated libs

--- lib/awesome_elixir_web/tasks/md_parser_task.ex
+++ lib/awesome_elixir_web/tasks/md_parser_task.ex
@@ -5,6 +5,7 @@ defmodule AwesomeElixirWeb.MdParserTask do
   """
 
   @github_host "https://github.com"
+  @outdated_days 365
 
   def run(markdown) do
     {:ok, raw_html, _} = markdown |> Earmark.as_html
@@ -40,12 +41,26 @@ defmodule AwesomeElixirWeb.MdParserTask do
       Floki.traverse_and_update(html, fn
         {"li", [], [{"a", [{"href", ^url}], text}, desc]} ->
           %{stars: stars, last_commit: days} = data
-          new_tag = {"span", [], ["#{stars} ⭐, 📅 #{days} days ago"]}
-          {"li", [], [{"a", [{"href", url}], text}, desc, new_tag]}
+          {
+            "li", html_link_attrs(days),
+            [
+              {"a", [{"href", url}], text},
+               desc,
+               star_tag(stars),
+               commit_tag(days)
+            ]
+          }
 
         other -> other
       end)
     end)
+  end
+
+  defp star_tag(stars), do: {"span", [], ["#{stars} ⭐"]}
+  defp commit_tag(days), do: {"span", [], ["📅 #{days} days ago"]}
+
+  defp html_link_attrs(days) do
+    if days >= @outdated_days, do: [{"class", "outdated"}], else: []
   end
 
   defp parse_links(document) do
